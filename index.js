e52bb11b469153ee862df133d93e0d8bf5b1bad0
Peter Frangatzis <pfraggatzis2002@gmail.com>
2024-12-28 21:28:15 +0200

    index.js: Add endpoint for uploading audio files
    Multer saves files POST'ed to /uploads in ./public/assets/uploads.  The
    HTTP response contains the name of the uploaded file.
    
    NOTE: file type correction should preferably happen at the *frontend*.

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const express = require("express");
 const app = express();
 const port = 6969;
 
+// Set up Multer and configure to store uploaded stuff in ./public/assets/uploads
+const multer = require("multer");
+const upload = multer({
+	dest: "public/assets/uploads",
+	// TODO: Configure filename
+});
+
 // Middleware:
 // 1. Serve static content from ./public (use it as content root aka /)
 // 2. Enable parsing for json and url-encoded content (because why not?)
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Endpoint for uploading audio/song files
+app.post("/uploads", upload.single("custom_audio"), (req, res, next) => {
+	if (!req.file)
+		return res.status(400).send(JSON.stringify({ msg: "No file uploaded!" }));
+
+	// NOTE: More sanity checks wouldn't hurt here...
+
+	console.log(req.file, "\n", req.body);
+
+	res.status(200).send(JSON.stringify({
+		msg: "Uploaded file successfully!",
+		filename: req.file.filename
+	}));
+});
 
 // Start listening for connections
 app.listen(port, () => {
